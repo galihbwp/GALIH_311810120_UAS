51940e801cc3a37736895897f20a7d4d687aca1d
Galih Bagus Wiguna Paksi <paksiwigunagalih@gmail.com>
2021-07-22 21:33:30 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/pelitabangsa/galih_311810120_uas/LoginActivity.java
@@ -0,0 +1,14 @@
+package id.ac.pelitabangsa.galih_311810120_uas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
